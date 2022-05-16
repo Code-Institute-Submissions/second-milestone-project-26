f7900824614977b7396048a752c0c3405a9bfa51
Miles-Cownie <miles_cownie94@protonmail.com>
2022-05-16 14:32:44 +0000

    Add JS function to play sound when button is clicked and toggle sound on/off

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 let backgroundSlider = document.getElementById('backgroundSlider');
 let backgroundMusic = document.getElementById('backgroundMusic');
 
-backgroundSlider.addEventListener("click", function () {
+backgroundSlider.addEventListener('click', function () {
     if (backgroundMusic.paused) {
         backgroundMusic.play();
         backgroundMusic.volume = 0.25;
@@ -11,3 +11,26 @@ backgroundSlider.addEventListener("click", function () {
         backgroundMusic.pause();
     }
 });
+
+// Function to toggle mute for answer sounds
+let answerSound = document.getElementById('answerSound');
+let soundsSlider = document.getElementById('soundsSlider');
+
+soundsSlider.addEventListener('click', function () {
+    if (answerSound.muted === true) {
+        answerSound.muted = false;
+    } else {
+        answerSound.muted = true;
+    }
+});
+
+// Plays sound when user clicks their answer
+
+let btns = document.getElementsByClassName('option');
+
+for (i of btns) {
+    i.addEventListener ('click', function() {
+        answerSound.play();
+    });
+}
+
