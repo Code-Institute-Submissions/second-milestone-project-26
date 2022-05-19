29b129df9c76adb07dc195ab270fcdedd2e2f631
Miles-Cownie <miles_cownie94@protonmail.com>
2022-05-19 10:05:35 +0000

    MAJOR CHANGE, Change quiz.html tags to better accomodate JS, change JS to reduce code outside function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,139 @@
-//JS code for the audio play and control functions
+const questionText = document.getElementById('text-content');
+const op1Btn = document.getElementById('op1');
+const op2Btn = document.getElementById('op2');
+const op3Btn = document.getElementById('op3');
+const musicButton = document.getElementById('backgroundSlider');
+const soundButton = document.getElementById('soundsSlider');
+const userProgress = document.getElementById('user-progress');
 
-// Function to toggle background music
+musicButton.addEventListener('click', toggleMusic);
+soundButton.addEventListener('click', toggleSound);
+op1Btn.addEventListener('click', playSound, checkAnswer);
+op2Btn.addEventListener('click', playSound, checkAnswer);
+op3Btn.addEventListener('click', playSound, checkAnswer);
 
-let backgroundSlider = document.getElementById('backgroundSlider');
-let backgroundMusic = document.getElementById('backgroundMusic');
+let currentQuestion = 0;
+var score = 0;
+var newQuestionList;
 
-backgroundSlider.addEventListener('click', function () {
+let easyQuestions = [
+        {
+            question: 'Dummy Question A',
+            answers: [
+                {option: 'Dummy Answer A1', answer: true},
+                {option: 'Dummy Answer A2', answer: false},
+                {option: 'Dummy Answer A3', answer: false}
+            ]
+        },
+        {
+            question: 'Dummy Question B',
+            answers: [
+                {option: 'Dummy Answer B1', answer: false},
+                {option: 'Dummy Answer B2', answer: true},
+                {option: 'Dummy Answer B3', answer: false}
+            ]
+        },
+        {
+            question: 'Dummy Question C',
+            answers: [
+                {option: 'Dummy Answer C1', answer: true},
+                {option: 'Dummy Answer C2', answer: false},
+                {option: 'Dummy Answer C3', answer: false}
+            ]
+        },
+        {
+            question: 'Dummy Question D',
+            answers: [
+                {option: 'Dummy Answer D1', answer: true},
+                {option: 'Dummy Answer D2', answer: false},
+                {option: 'Dummy Answer D3', answer: false}
+            ]
+        },
+        {
+            question: 'Dummy Question E',
+            answers: [
+                {option: 'Dummy Answer E1', answer: true},
+                {option: 'Dummy Answer E2', answer: false},
+                {option: 'Dummy Answer E3', answer: false}
+            ]
+        },
+]
+
+function createRandomQuiz() {
+    for (let i = easyQuestions.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i)
+        let k = easyQuestions[i]
+        easyQuestions[i] = easyQuestions[j]
+        easyQuestions[j] = k
+    }
+    newQuestionList = easyQuestions.slice(0, 2);
+}
+
+function beginQuiz (){
+    createRandomQuiz();
+    currentQuestion = 0;
+    userProgress = 0;
+    questionText.innerHTML = newQuestionList[currentQuestion].question;
+    op1Btn.innerHTML = newQuestionList[currentQuestion].answers[0].option;
+    op2Btn.innerHTML = newQuestionList[currentQuestion].answers[1].option;
+    op3Btn.innerHTML = newQuestionList[currentQuestion].answers[2].option;
+}
+
+function checkAnswer() {
+    if (this === op1Btn) {
+        if (newQuestionList[currentQuestion].answers[0].answer) {
+            op1Btn.classList.add('correct');
+            score++;
+        } else {
+            op1Btn.classList.add('incorrect');
+        }
+        userProgress++;
+        showAnswer();
+    } else if (this === op2Btn) {
+        if (newQuestionList[currentQuestion].answers[0].answer) {
+            op2Btn.classList.add('correct');
+            score++;
+        } else {
+            op2Btn.classList.add('incorrect');
+        }
+        userProgress++; 
+        showAnswer();
+    } else if (this === op3Btn) {
+        if (newQuestionList[currentQuestion].answers[0].answer) {
+            op3Btn.classList.add('correct');
+            score++;
+        } else {
+            op3Btn.classList.add('incorrect');
+        }
+        userProgress++;
+        showAnswer();
+    } else {
+        alert ('Something borked, try again!')
+    }
+}
+
+beginQuiz();
+
+
+// Functions to control background music and answer sounds
+
+function toggleMusic() {
     if (backgroundMusic.paused) {
         backgroundMusic.play();
         backgroundMusic.volume = 0.25;
     } else {
         backgroundMusic.pause();
     }
-});
+}
 
-// Function to toggle mute for answer sounds
-let answerSound = document.getElementById('answerSound');
-let soundsSlider = document.getElementById('soundsSlider');
-
-soundsSlider.addEventListener('click', function () {
+function toggleSound() {
     if (answerSound.muted === true) {
         answerSound.muted = false;
     } else {
         answerSound.muted = true;
     }
-});
-
-// Plays sound when user clicks an answer
-
-let btns = document.getElementsByClassName('option');
-
-for (i of btns) {
-    i.addEventListener('click', function () {
-        answerSound.play();
-    });
 }
 
-// JS code for quiz section
-
-document.addEventListener('DOMContentLoaded', function () {
-    shuffleQuizList();
-    createQuizArray();
-    showQuestion();
-});
-
-function startQuiz() {
-
-};
-
-function shuffleQuizList() {
-    for (let i = easyQuestionList.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * i)
-        let k = easyQuestionList[i]
-        easyQuestionList[i] = easyQuestionList[j]
-        easyQuestionList[j] = k
-    }
-};
-
-function createQuizArray() {
-    let i = easyQuestionList.slice(0, 2);
-    newQuestionList = i
-    console.log(newQuestionList);
-};
-
-function showQuestion() {
-    let questionBox = document.getElementById('textContent');
-    for (let i = 0; i < newQuestionList.length - 1; i++) {
-        questionBox.innerHTML = `<p>${newQuestionList[i].question}</p>`;
-    };
-};
-
-function showOptions() {
-
-};
-
-function checkAnswer() {
-
-};
-
-function showAnswer() {
-
-};
-
-// Variables
-
-let newQuestionList = [];
-
-//Questions List - Easy
-
-const easyQuestionList = [{
-        question: "Dummy question 1",
-        answers: {
-            a: 'Dummy answer 1a',
-            b: 'Dummy answer 1b',
-            c: 'Dummy answer 1c'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "Dummy question 2",
-        answers: {
-            a: 'Dummy answer 2a',
-            b: 'Dummy answer 2b',
-            c: 'Dummy answer 2c'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "Dummy question 3",
-        answers: {
-            a: 'Dummy answer 3a',
-            b: 'Dummy answer 3b',
-            c: 'Dummy answer 3c'
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "Dummy question 4",
-        answers: {
-            a: 'Dummy answer 4a',
-            b: 'Dummy answer 4b',
-            c: 'Dummy answer 4c'
-        },
-        correctAnswer: 'c'
-    }
-];
+function playSound() {
+    answerSound.play()
+}
