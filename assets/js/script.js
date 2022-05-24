ce65590d4f831edebf99de0fd8eb008be911cd90
Miles-Cownie <miles_cownie94@protonmail.com>
2022-05-24 14:59:50 +0000

    Add additional JS comments and clarify code which was built using tutorial

--- assets/js/script.js
+++ assets/js/script.js
@@ -777,6 +777,10 @@ window.addEventListener('DOMContentLoaded', () => {
         checkAudio();
         questionCounter = 0;
         score = 0;
+        /* 
+        Checks the page difficulty setting and pulls questions from the appropriate roster.
+        If page does not have setting then game instructions are displayed
+        */
         level = difficulty.dataset.number;
         if (level === '1') {
             availableQuestions = [...easyQuestions];
@@ -799,7 +803,12 @@ window.addEventListener('DOMContentLoaded', () => {
     // Move to next question function
 
     function getNewQuestion() {
-        // Code to execute when the user has answered all the available questions
+        /* 
+        Code to execute when the user has answered all the available questions. 
+        Displays user score against the total number of questions asked.
+        Hide's extra buttons and offer's user option to try again.
+        Edited from Brian Design tutorial
+        */
         if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
             question.innerText = `Congratulations! You answered ${score} out of ${maxQuestions} correctly!`;
             progressText.innerText = `That's all the questions this time!`;
@@ -812,19 +821,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
             // Code to execute until all questions are answered
 
-            // Increments the question counter and updates progress bar
+            // Increments the question counter and updates progress bar - edited from Brian Design tutorial
             questionCounter++;
             progressText.innerText = `You are on question ${questionCounter} out of ${maxQuestions}`;
             progressBar.style.width = `${(questionCounter/maxQuestions) * 100}%`;
 
-            // Randomises the question list
+            // Randomises the question list - from Brian Design tutorial
 
             const questionIndex = Math.floor(Math.random() * availableQuestions.length);
 
             currentQuestion = availableQuestions[questionIndex];
             question.innerText = currentQuestion.question;
 
-            // Applies answer choices to the option buttons
+            // Applies answer choices to the option buttons - from Brian Design tutorial
 
             choices.forEach(choice => {
                 const number = choice.dataset.number;
@@ -837,26 +846,27 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Adds event listeners to the option buttons
+    // Adds event listeners to the option buttons - from Brian Design tutorial
 
     choices.forEach(choice => {
         choice.addEventListener('click', e => {
             playSound();
             if (!acceptingAnswers) return;
-            // Checks if selected answer is correct
+            // Checks if selected answer is correct - edited from Brian Design tutorial
             acceptingAnswers = false;
             const selectedChoice = e.target;
             const selectedAnswer = selectedChoice.dataset.number;
 
             let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+            // Displays message depending on whether user answer is correct or incorrect 
             if (classToApply === 'correct') {
                 score++;
                 question.innerText = `Great Job! The correct answer was ${currentQuestion.answerText}`;
             } else if (classToApply !== 'correct') {
                 question.innerText = `Afraid not! the correct answer was ${currentQuestion.answerText}`;
             }
-            // Applies correct or incorrect class to the selected option
+            // Applies correct or incorrect class to the selected option - Edited from Brian Design tutorial
             selectedChoice.classList.add(classToApply);
 
             setTimeout(function () {
@@ -870,34 +880,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Functions to control background music and answer sounds
 
+    // Starts and Pauses the background music on user click
+
     function toggleMusic() {
         let backgroundMusic = document.getElementById('background-music');
         if (backgroundMusic.paused) {
             backgroundMusic.play();
             backgroundMusic.volume = 0.1;
+            //Sets the session storage to remember user choice from page to page 
             window.sessionStorage.setItem('bckgrdMusic', 'true');
         } else {
             backgroundMusic.pause();
+            //Sets the session storage to remember user choice from page to page
             window.sessionStorage.setItem('bckgrdMusic', 'false');
         }
     }
+    
+    // Toggles mute for the answer sounds
 
     function toggleSound() {
         let answerSound = document.getElementById('answer-sound');
         if (answerSound.muted === true) {
             answerSound.muted = false;
             answerSound.volume = 0.1;
+            //Sets the session storage to remember user choice from page to page
             window.sessionStorage.setItem('answerSound', 'true');
         } else {
             answerSound.muted = true;
+            //Sets the session storage to remember user choice from page to page
             window.sessionStorage.setItem('answerSound', 'false');
         }
     }
+
+    // Plays answer sound on user click
 
     function playSound() {
         let answerSound = document.getElementById('answer-sound');
         answerSound.play();
     }
+
+    // Checked the Audio settings against the session storage, sets the switch toggles and audio to user's selection.
+
 
     function checkAudio() {
         let soundInput = document.getElementById('sound');
